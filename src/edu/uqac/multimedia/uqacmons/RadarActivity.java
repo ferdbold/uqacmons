4600f8db9edcab2c0bd1ca28ccdf893f7fae6104
Alex <alex3521@hotmail.fr>
2014-04-14 15:21:46 -0400

    Added an circle animation on the Flash animation. have to test on real device cause of lag

--- src/edu/uqac/multimedia/uqacmons/RadarActivity.java
+++ src/edu/uqac/multimedia/uqacmons/RadarActivity.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -15,6 +20,7 @@ public class RadarActivity extends Activity {
 	private ImageButton uqacpedia;
 	private ImageView blueBip;
 	private ImageView redBip;
+	private ImageView redCircle;
 	private Button flash;
 	
 	@Override
@@ -25,10 +31,12 @@ public class RadarActivity extends Activity {
 		uqacpedia = (ImageButton)findViewById(R.id.b_uqacpedia);
 		blueBip = (ImageView)findViewById(R.id.blueBip);
 		redBip = (ImageView)findViewById(R.id.redBip);
+		redCircle = (ImageView)findViewById(R.id.redCircle);
 		flash = (Button)findViewById(R.id.b_flash);
 		
 		//Set variables
 		redBip.setAlpha(0F);
+		redCircle.setAlpha(0F);
 		
 		uqacpedia.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -53,11 +61,28 @@ public class RadarActivity extends Activity {
 	}
 	
 	public void Flash() {
+		//Red Bip Alpha
 		redBip.setAlpha(1F);
-		AlphaAnimation alpha = new AlphaAnimation(1F, 0.0F); // change values as you want
-		alpha.setDuration(1000); // Make animation instant
-		alpha.setFillAfter(true); // Tell it to persist after the animation ends
-		redBip.startAnimation(alpha);// And then on your imageview
+		AlphaAnimation alpha = new AlphaAnimation(1F, 0.0F); // Alpha variation
+		alpha.setDuration(1000); // time in milliseconds
+		alpha.setFillAfter(true); // Stays Like it is at the end of the animation
+		redBip.startAnimation(alpha);// Launch animation
+		//Red circe Animation
+		redCircle.setAlpha(1F); // Alpha animation first
+		ScaleAnimation scale = new ScaleAnimation(1F, 2F, // Start and end values for the X axis scaling
+	            1F, 2F, // Start and end values for the Y axis scaling
+	            Animation.RELATIVE_TO_SELF, 0.5F, // Pivot point of X scaling
+	            Animation.RELATIVE_TO_SELF, 0.5F); // Pivot point of Y scaling
+		scale.setDuration(2000);
+		scale.setFillAfter(true);
+		// On utilise un set d'animation pour les faire en meme temps
+		AnimationSet animSet = new AnimationSet(true);
+	    animSet.setFillAfter(true);
+	    animSet.setDuration(1000);
+	    animSet.setInterpolator(new LinearInterpolator());
+	    animSet.addAnimation(alpha);
+	    animSet.addAnimation(scale);
+	    redCircle.startAnimation(animSet); //Launch Animations
 	}
 
 }
